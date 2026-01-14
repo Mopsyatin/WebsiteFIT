12e853163898117e8f2fb110dc82b844e2c96936
Mikhael <131536729+Mopsyatin@users.noreply.github.com>
2026-01-14 21:49:10 +0000

    Basic Spring Boot project setup
    - Generated a base Spring Boot project
    - Added default configuration and dependencies
    - Set up project structure

---
+++ src/test/java/ie/floral/flower_website/FlowerWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.floral.flower_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
