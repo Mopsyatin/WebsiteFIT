cb966c2668e02cb222b2726ced4980a6c876a828
Mikhael <131536729+Mopsyatin@users.noreply.github.com>
2026-01-15 12:35:19 +0000

    Revert "-Upgraded project structure"
    This reverts commit cd960805350f57945c89ce4077779c439244ee46.

---
+++ README.md
@@ -0,0 +1,1 @@
+# WebsiteFIT
--- src/main/java/ie/floral/flower_website/FlowerWebsiteApplication.java
+++ src/main/java/ie/floral/flower_website/FlowerWebsiteApplication.java
@@ -1,17 +1,19 @@
 package ie.floral.flower_website;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlowerWebsiteApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(FlowerWebsiteApplication.class);
 
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
 		SpringApplication.run(FlowerWebsiteApplication.class, args);
 	}
-
 
 
 }
