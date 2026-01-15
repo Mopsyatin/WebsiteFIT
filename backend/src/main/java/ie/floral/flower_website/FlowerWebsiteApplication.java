bd5f91e3917ffb461dc7ce642ea3e43e122d99fc
Mikhael <131536729+Mopsyatin@users.noreply.github.com>
2026-01-15 12:37:00 +0000

    Rebuild Project structure
    -rebuilt project structure

---
+++ backend/src/main/java/ie/floral/flower_website/FlowerWebsiteApplication.java
@@ -0,0 +1,17 @@
+package ie.floral.flower_website;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerWebsiteApplication {
+
+
+    static void main(String[] args) {
+		SpringApplication.run(FlowerWebsiteApplication.class, args);
+	}
+
+
+
+}
